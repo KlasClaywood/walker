a318ad74f0bd2439a5b14a55562c993a78f17f5b
KlasClaywood <klas.claywood@gmail.com>
2016-10-25 10:38:48 +0200

    Positiondata

---
+++ Console-Walker/Position.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Walker
+{
+    class Position
+    {
+        public int X { set; get; }
+        public int Y { set; get; }
+        public Tile Tile { private set; get; }
+        public Position (int x, int y, Tile tile)
+        {
+            this.Tile = tile;
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
