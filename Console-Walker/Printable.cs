07d9c9f95f2a0e2ed1d556cfd1921a19f203149b
KlasClaywood <klas.claywood@gmail.com>
2016-10-25 10:31:59 +0200

    Alla viktiga delar

---
+++ Console-Walker/Printable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Walker
+{
+    class Printable
+    {
+        public Position Position { set; get; }
+        public Printable(int x, int y, Tile tile)
+        {
+            this.Position = new Position(x, y, tile);
+        }
+
+    }
+}
