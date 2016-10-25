f022334c6291fe30da4cb234cf2124eb13a3f6df
KlasClaywood <klas.claywood@gmail.com>
2016-10-25 10:38:39 +0200

    Tiledata

---
+++ Console-Walker/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Walker
+{
+    class Tile
+    {
+        public char Char { private set; get; }
+        public ConsoleColor Color { private set; get; }
+        public Tile (char c, ConsoleColor col)
+        {
+            this.Char = c;//klas
+
+            this.Color = col;
+
+        }
+    }
+}
